4fb4effbb1ee1f842ab8197c528a15a470564168
Laurent Galassi <link684@msn.com>
2018-03-11 11:59:27 +0100

    Optimisation du code déjà présent
    read/write du ppm binaire et pas binaire à faire

--- TatouageImage/TatouageImage/main.cpp
+++ TatouageImage/TatouageImage/main.cpp
@@ -19,7 +19,15 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
-//#include "readpgm.h"
+
+typedef struct {
+	unsigned char red, green, blue;
+} PPMPixel;
+
+typedef struct {
+	int x, y;
+	PPMPixel *data;
+} PPMImage;
 
 #define MAXROWS 512
 #define MAXCOLS 512
@@ -119,23 +127,11 @@ int pgmRead(char *fileName, long *rows, long *cols,
 		return (0);
 	}
 
-	char tmp[MAXROWS][MAXCOLS];
-	for (int i = 0; i < MAXROWS; i++)
-	{
-		for (int j = 0; j < MAXCOLS; j++)
-		{
-			tmp[i][j] = (char)image[i][j];
-		}
-	}
-
 	/* Read in the data for binary (P5) or ascii (P2) PGM formats   */
 	if (binary) {
 		for (i = 0; i < (*rows); i++) {
 			for (j = 0; j < (*cols); j++) {
-				/* pas sûr que ça marche donc je garde l'original en comm
-				numberRead += fread((void *)&(image[i][0]),
-					sizeof(unsigned char), (*cols), filePointer);*/
-				filePointer.read(&(tmp[i][j]), sizeof(unsigned char));
+				filePointer >> image[i][j];
 				if (filePointer.eof()) break;
 			}
 		}
@@ -148,14 +144,6 @@ int pgmRead(char *fileName, long *rows, long *cols,
 				image[i][j] = (unsigned char)temp;
 			}
 			if (temp == EOF) break;
-		}
-	}
-
-	for (int i = 0; i < MAXROWS; i++)
-	{
-		for (int j = 0; j < MAXCOLS; j++)
-		{
-			image[i][j] = (unsigned char)tmp[i][j];
 		}
 	}
 
@@ -177,7 +165,7 @@ int pgmRead(char *fileName, long *rows, long *cols,
 */
 int pgmWrite(char* filename, long rows, long cols,
 	unsigned char image[MAXROWS][MAXCOLS], char* comment_string) {
-	FILE* file;        /* pointer to the file buffer */
+	ofstream file;        /* pointer to the file buffer */
 	int maxval;        /* maximum value in the image array */
 	long nwritten = 0; /* counter for the number of pixels written */
 	long i, j;          /* for loop counters */
@@ -189,51 +177,34 @@ int pgmWrite(char* filename, long rows, long cols,
 	}
 
 	/* open the file; write header and comments specified by the user. */
-	fopen_s(&file, filename, "w");
-	fprintf(file, "P5\n");
-	/*
 	file.open(filename);
 	if (file.fail())
 	{
 		cout << "ERROR: file open failed, incorrect file name" << endl;
 		return 0;
 	}
-	file << "P5\n";*/
+	file << "P5\n";
 
 	if (comment_string != NULL)
 	{
-		//file << "# " << comment_string << "%s \n";
-		fprintf(file, "# %s \n", comment_string);
+		file << "# " << comment_string << "\n";
 	}
 
 	/* write the dimensions of the image */
-	//file << cols << " " << rows << endl;
-	fprintf(file, "%ld %ld \n", cols, rows);
+	file << cols << " " << rows << endl;
 
 	/* NOTE: MAXIMUM VALUE IS WHITE; COLOURS ARE SCALED FROM 0 - */
 	/* MAXVALUE IN A .PGM FILE. */
 
 	/* WRITE MAXIMUM VALUE TO FILE */
-	//file << (int)255 << endl;
-	fprintf(file, "%d\n", (int)255);
+	file << (int)255 << endl;
 
 	/* Write data */
-
-	/* for (i=0; i < rows; i++) {
-	Orig: nwritten += fwrite((void*)&(image[i][0]),sizeof(unsigned char), cols, file);}*/
 	for (i = 0; i < rows; i++)
 		for (j = 0; j < cols; j++)
-			//file << &(image[i][j]);
-			//file.write(&(image[i][j]), sizeof(unsigned char));
-			fwrite(&(image[i][j]), sizeof(unsigned char), 1, file);
+			file << image[i][j];
 
-
-
-	/*     printf ("\nNumber of pixels total(from rows*cols): %ld\n",rows*cols); */
-	/*       printf ("Number of pixels written: %ld\n\n",nwritten); */
-
-	//file.close();
-	fclose(file);
+	file.close();
 	return(1);
 }
 
